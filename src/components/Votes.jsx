69b5c2c5bbef31bb73432d373d864aa8d0f01f92
YoriB <deyoribad@yahoo.co.uk>
2023-02-09 15:59:58 +0000

    lost files

---
+++ src/components/Votes.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { patchArticleApi } from "../utils/api";
+
+const Votes =({votes, singleArticleID}) => {
+  const [voteChange, setVoteChange] = useState(0);
+
+const incVotes = () => {
+  setVoteChange((currChange) => currChange + 1);
+
+  patchArticleApi(singleArticleID, 1);
+}
+
+const decVotes = () => {
+  setVoteChange((currChange) => currChange - 1);
+
+  patchArticleApi(singleArticleID, -1);
+}
+
+return (
+
+  <div>
+    <button disabled={voteChange ===1}  onClick={() => incVotes()}>Like</button>
+    <span> Likes : {votes + voteChange}</span>
+    <button disabled={voteChange ===-1} onClick={() => decVotes()}>Unlike</button>
+  </div>
+)
+}
+export default Votes;
