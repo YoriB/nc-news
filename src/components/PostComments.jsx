97f4fbae28543fa76a86032d14a32c727dfdf7b2
YoriB <deyoribad@yahoo.co.uk>
2023-03-01 20:53:58 +0000

    task11

--- src/components/PostComments.jsx
+++ src/components/PostComments.jsx
@@ -1,54 +1,45 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import { postCommentApi } from '../utils/api';
 
+const PostComments = ({ singleArticleID, setComments }) => {
+  const [body, setBody] = useState('');
+  const [isCommentSubmitted, setIsCommentSubmitted] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
 
-const PostComments = ({singleArticleID, setComments}) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (body) {
+      setIsCommentSubmitted(true);
+      setIsDisabled(true);
+      postCommentApi( body, singleArticleID).then((commentsFromApi) => {
+        setComments((currComments) => [...currComments, commentsFromApi]);
+      });
+    } else {
+      // display error message
+    }
+  };
 
-const [body, setBody] = useState('');
-const [isCommentSubmitted, setIsCommentSubmitted] = useState(false);
-const [isDisabled, setIsDisabled] = useState(false);
-
-const handleSubmit = (event) => {
-
-event.preventDefault(); 
-
-if (body) {
-  // form is valid, submit the form 
-  setIsCommentSubmitted(true);
-  setIsDisabled(true);
-} else {
-  // form is invalid, display an error message
- 
-}
-
-postCommentApi(body, singleArticleID).then(commentsFromApi => {
-  
-  setComments((currComments) => [...currComments, commentsFromApi]).then(() => {  
- 
-  });
-})
-}
-
-return (
-<section> 
-<h2 id="underlined">POST A COMMENT</h2>
-<form onSubmit={handleSubmit}> 
-<label htmlFor=' comment'>Post Comment </label>
-<textarea
-id = 'comment-box'
-rows = '6'
-type="text"
-placeholder="write comment here"
-onChange={(event) => setBody(event.target.value)}
-/> 
-<button disabled={isDisabled} type="submit">Post</button>
-{isCommentSubmitted && alert('Form submitted successfully')}
-{!isCommentSubmitted && <p className='comment-instruction'>Please enter a comment</p>}
-
-</form>
-</section> 
-)
-}
+  return (
+    <section>
+      <h2 id="underlined">POST A COMMENT</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="comment">Post Comment </label>
+        <textarea
+          id="comment-box"
+          rows="6"
+          type="text"
+          placeholder="write comment here"
+          onChange={(event) => setBody(event.target.value)}
+        />      
+        <button disabled={isDisabled} type="submit">
+          Post
+        </button>
+        {isCommentSubmitted && alert('Comment submitted successfully')}
+        {!isCommentSubmitted && <p>Please enter a comment and your username</p>}
+      </form>
+    </section>
+  );
+};
 
 export default PostComments;
 
